b9d1e3829b594f558e6bcae20e685fbaaaa72f71
snap78 <145050116+snap78@users.noreply.github.com>
2023-09-15 14:47:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ window.onload = function() {
     if (button) {
         button.click();
         // Replace 'https://example.com' with your desired destination URL
-        window.location.href = "http://clickpavel2.social-networking.me/";
+        window.location.href = "https://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbkwzZEwyalNIdDRMdnpNclVpQ3hPeDB6bDM0UXxBQ3Jtc0trRVpyWlljU0xnaWozc3BVT3R3SndWclowdzBaRnU3cXVGMTBkSWVxY2lfUGtPLVZLT2RtZmJFdU9QZjhuamlkeHVfVFhvbkNDRU5rOEh5MDhzS2IzNWIzMVp3Qy1nemdCRHlMd0pVem1HZUFCSXM4SQ&q=http%3A%2F%2Fclickpavel2.social-networking.me%2F&html_redirect=1 ";
     }
 };
